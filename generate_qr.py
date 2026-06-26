6391e4ad524962a8d021b5589369f4959ebfa805
Shivam Gupta <86199904+Abraxas001@users.noreply.github.com>
2026-06-26 22:41:54 +0530

    Update generate_qr.py

--- generate_qr.py
+++ generate_qr.py
@@ -4,8 +4,10 @@ import qrcode
 YOUR_IP = "192.168.1.20"  # replace with your actual IP (run ipconfig)
 NUM_TABLES = 10
 
+YOUR_URL = "https://restaurant-ordering-q8te.onrender.com"
+
 for i in range(1, NUM_TABLES + 1):
-    url = f"http://192.168.1.8:8000/table/{i}"
+    url = f"{YOUR_URL}/table/{i}"
     img = qrcode.make(url)
     img.save(f"qr_table_{i}.png")
     print(f"Generated QR for table {i}")
