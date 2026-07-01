7378a9fe56794dd2b4baa2dc1d7d6c9199c715f0
Shivam Gupta <86199904+Abraxas001@users.noreply.github.com>
2026-07-01 14:26:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,8 +11,7 @@ from typing import Optional
 import hashlib
 import secrets
 from passlib.context import CryptContext
-
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
 
 def hash_password(password: str) -> str:
     return pwd_context.hash(password)
